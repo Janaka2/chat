65de2b965dbf55b541f404f502a04108f8cff192
System Administrator <root@Janakas-MacBook-Pro.local>
2019-07-14 18:22:39 +0200

    almost final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDKhqErW8y8Vzd5jM38-dQJCOxiWMTzgVM',
+    authDomain: 'chat-15295.firebaseapp.com',
+    databaseURL: 'https://chat-15295.firebaseio.com',
+    projectId: 'chat-15295',
+    storageBucket: 'chat-15295.appspot.com',
+    messagingSenderId: '844867362199',
+    appId: '1:844867362199:web:0528df9144c0f0b7'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,17 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDKhqErW8y8Vzd5jM38-dQJCOxiWMTzgVM',
+    authDomain: 'chat-15295.firebaseapp.com',
+    databaseURL: 'https://chat-15295.firebaseio.com',
+    projectId: 'chat-15295',
+    storageBucket: 'chat-15295.appspot.com',
+    messagingSenderId: '844867362199',
+    appId: '1:844867362199:web:0528df9144c0f0b7'
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
